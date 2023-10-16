b52ebeb8c3f2bd3e173e0296ef8f7180ae01f053
Jared Frazier <cscidev001@gmail.com>
2023-10-16 15:06:07 +0200

    finite mesh for 1d, 2d quadrilateral, and 3d hexadron elements

--- src/PDEMethods.jl
+++ src/PDEMethods.jl
@@ -3,5 +3,6 @@ module PDEMethods
 
 include("polynomials.jl")
 include("quadrature.jl")
+include("fem_mesh.jl")
 
 end # module PDEMethods
--- src/fem_mesh.jl
+++ src/fem_mesh.jl
@@ -1,27 +1,46 @@
 """
-    compute_global_coordinate_array(lx, dims::Int = 1, neles::Int = 1; ly, lz)
+    compute_global_coordinate_matrix(
+        lx, nelex::Int, dims::Int = 1; 
+        ly = 0, neley::Int = 0, 
+        lz = 0, nelez::Int = 0)
 
-Return array of shape [`dims`, `neles+1`]
+Return matrix of shape [`dims`, `nx` + `ny` + `nz`] where `nx = nelex + 1`,
+`ny = neley + 1`, and `nz = nelez + 1` that decribes the global physical
+coordinates of the desired domain of dimension `dims`.
 
-Need to think about general cases for nn and neles from Simpson2017 since...
+# Arguments
+- `lx`: Length of physical x-dimension.
+- `nelex`: Number of finite elements in x-dimension.
+- `dims`: Dimensions of domain.
+- `ly = 0`: Same as `lx` but for y-dimension.
+- `neley::Int = 0`: Same `nelex` but for y-dimension.
+- `lz = 0`: Same as `lx` but for z-dimension.
+- `nelez::Int = 0`: Same as `nelex` but for z-dimension
 
 # References
-[1] : ch. 3.4.1, 5.2, and 6.3 Simpson2017
+[1] : Ch. 3.4.1, 5.2 (see fig 5.3), and 6.3 of Simpson2017 for conceptual
+inspiration, and Ch. 5.6 of Simpson2017 for implementation details.
 """
-function compute_global_coordinate_array(
-    lx::Int, neles::Int, dims::Int = 1; ly::Int, lz::Int)
+function compute_global_coordinate_matrix(
+    lx, nelex::Int, dims::Int = 1; 
+    ly = 0, neley::Int = 0, 
+    lz = 0, nelez::Int = 0)
     @assert dims in [1, 2, 3] "`dims` must be 1D, 2D, or 3D (i.e., [1, 2, 3])"
-    @assert neles > 0 "`neles` must be natural number" 
+
+    # initialize global coordinate/connectivity matrix
+    g_coord = nothing
  
-    # initialize global cooordinate/connectivity array
-    n_nodes = neles + 1
-    g_coord = zeros(Int, dims, n_nodes)
+    # compute number of nodes in each direction
+    nx = nelex + 1
+    ny = neley + 1
+    nz = nelez + 1
 
-    # compute x-nodal info
-    dx = lx ÷ neles # dist between subseq nodes in x-dir
-    nx = compute_num_nodes(lx, dx, neles)
+    # compute distance between each node in each direction
+    dx = lx/nelex
+    dy = lx/neley
+    dz = lx/nelez
 
-    # constants for ind
+    # constants for indexing and name for dimensions
     x = one_d = 1
     y = two_d = 2
     z = three_d = 3
@@ -29,18 +48,16 @@ function compute_global_coordinate_array(
     # global node counter
     node_counter = 1
 
-    throw("notimplemented")
-
-    # create connectivity arrays based on dimensions of domain
-    if dims == one_d
+    # create connectivity matrix based on dimensions of domain
+    if dims == one_d 
+        g_coord = zeros(one_d, nx)
         for i = 1:nx
             g_coord[x, node_counter] = (i - 1)*dx
             node_counter += 1
         end  
 
     elseif dims == two_d
-        dy = 
-        ny = 
+        g_coord = zeros(two_d, nx + ny)
         for i = 1:nx, j = 1:ny 
             g_coord[x, node_counter] = (i - 1)*dx
             g_coord[y, node_counter] = (j - 1)*dy
@@ -48,22 +65,15 @@ function compute_global_coordinate_array(
         end 
 
     elseif dims == three_d 
-        ny = ly ÷ neles
-        nz = lz ÷ neles
-        for i = 1:nx, j = 1:ny, z = 1:nz
-            
+        g_coord = zeros(three_d, nx + ny + nz)
+        for i = 1:nx, j = 1:ny, k = 1:nz
+            g_coord[x, node_counter] = (i - 1)*dx
+            g_coord[y, node_counter] = (j - 1)*dy
+            g_coord[z, node_counter] = (k - 1)*dz
+            node_counter += 1
         end        
     end
  
     return g_coord
 end
 
-"""
-    compute_num_nodes(l_dim, d_dim, neles)
-
-Return number of nodes for a dimension of length `l_dim`, a distance between
-nodes in the element `d_dim`, and a number of finite elements `neles`.
-Accounts for `neles == 1` since in this case, 2 nodes should be used instead
-of just 1 node.
-"""
-compute_num_nodes(l_dim, d_dim, neles) = (l_dim ÷ d_dim) + (neles == 1 ? 1 : 0) 
