c6f8f9d589dec66077a71061d324fb13eaa5ec2f
Jared Frazier <cscidev001@gmail.com>
2023-10-19 15:16:03 +0200

    inplace gaussian elimeination

--- src/PDEMethods.jl
+++ src/PDEMethods.jl
@@ -4,5 +4,6 @@ module PDEMethods
 include("polynomials.jl")
 include("quadrature.jl")
 include("fem_mesh.jl")
+include("direct_methods.jl")
 
 end # module PDEMethods
--- src/direct_methods.jl
+++ src/direct_methods.jl
@@ -1,4 +1,37 @@
 # Direct methods for sparse linear systems 
 # should annihilate sparse entries (minimum degree algorithms)
-# * LU 
+# * LU (Gaussian Elimination)
 # * Cholesky Factorization
+
+"""
+    gaussian_elimination!(A)
+
+Return LU factorization of matrix `A` into lower triangular matrix `L` and upper 
+triangular matrix `U`. If `inplace = true`, then the subdiagonal entries
+of `A` are the lower triangular matrix `L` while the diagonal and superdiagonal
+entries of `A` are the upper triangular matrix `U`.
+
+# References 
+[1] : Heath algorithm 2.3
+"""
+function gaussian_elimination!(A)
+    m, n = size(A)
+    A_ = A
+    for k = 1:n-1
+        A_[k, k] == 0 && break
+        for i = k+1:n
+            multiplier =  A_[i, k]/A_[k, k]
+            A_[i, k] = multiplier 
+        end 
+
+        for j = k+1:n
+            for i = k+1:n
+                submatrix_transform = A_[i, j] - A_[i, k]*A_[k, j] 
+                A_[i, j] = submatrix_transform 
+            end
+        end
+    end
+    return nothing
+end 
+
+
