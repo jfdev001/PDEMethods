51f2424ccb66991162c62fd056a56d7d66def890
Jared Frazier <cscidev001@gmail.com>
2023-11-20 17:21:17 +0100

    init poisson sys matrix... though not strictly necessary for something like gauss-seidel etc, would be needed for multigrid (not finished)

--- src/finite_difference.jl
+++ src/finite_difference.jl
@@ -1,5 +1,9 @@
 # Finite difference schemes
 # see Driscoll 5.4 and Heath 8.6
+
+using LinearAlgebra: diagind
+
+
 function centered_first_deriv_wrt_pos(U, i, j, h) 
     (U[i+1,j] - U[i-1,j])/(2*h)
 end 
@@ -113,3 +117,126 @@ function laplace_eq_init_arrays(;grid_dim_n::Int=4, lbc=0, rbc=0, tbc=1, bbc=0)
 
     return mesh, A, b
 end
+
+"""
+
+Return `mesh`, differentiation matrix `A` and rhs `b` (aka f)
+
+```math
+\\begin{aligned}
+    D_{xy} &= \\frac{-2}{Δx^2} + \\frac{-2}{Δy^2} \\\\
+    D_x    &= \\frac{1}{Δx^2} \\\\
+    D_y    &= \\frac{1}{Δy^2}
+\\end{aligned}
+```
+
+Note that the diagonals of the differentiation matrix correspond to the 
+entries `u_{ij}` of dofs vector of form
+
+```math
+\\begin{bmatrix}
+u_{11} \\\\
+u_{12} \\\\
+\\vdots \\\\
+u_{21} \\\\
+\\vdots \\\\
+u_{N_x, N_y}
+\\end{bmatrix},
+```
+
+so that differentiation matrix A handles
+
+```math
+\\begin{bmatrix}
+u_{11}  & u_{12}  & \\cdots \\\\
+u_{11}  & u_{12}  & u_{13} & \\cdots \\\\
+\\vdots & \\cdots & \\vdots \\\\
+\\cdots & \\cdots & u_{N_x, N_y} 
+\\end{bmatrix}
+```
+
+TODO: How to evaluate `f` at nodal points?
+
+# References
+[1] : Equation (84) from p. 37 of Pawar2019.
+"""
+function poisson_eq_init_arrays(
+    f = (x, y) -> -8*π^2*sin(2*π*x)*sin(2*π*y) - 8*π^2*sin(32*π*x)*sin(32*π*y); 
+    grid_dim_n::Int=4, lbc=0, rbc=0, tbc=1, bbc=0)
+
+    # initialize mesh and boundary conditions
+    mesh = zeros(grid_dim_n, grid_dim_n)
+    mesh[:, 1] .= lbc   # left boundary condition
+    mesh[:, end] .= rbc # right boundary condition
+    mesh[1, :] .= tbc   # top boundary condition
+    mesh[end, :] .= bbc # bottom boundary condition
+
+    # initialize interior point matrices (i.e., differential matrix
+    # and rhs vector
+    n_interior_points_in_one_direction = grid_dim_n - 2
+    n_nodes = n_interior_points_in_one_direction^2
+    A = zeros(
+        n_nodes,
+        n_nodes,)
+    b = zeros(n_nodes)
+    
+    # Construct differential coeffecients from second order central difference
+    # of poisson equation 
+    Δx = 1 # h=(xL - xR)/n, e.g.,xL=10, xR=0, h = 10-0/10 = 1, Driscol fig 10.3.1 
+    Δy = 1
+
+    D_xy = -2/Δx^2 + -2/Δy^2  # D_xy * u_{ij}
+    D_x  = 1/Δx^2             # D_x  * (u_{i+1,j} + u_{i-1, j}) 
+    D_y  = 1/Δy^2             # D_y  * (u_{i, j+1} + u_{i, j-1})
+
+    # Get diagonal indices 
+    diag_ind = diagind(A, 0)
+    sup_diag_ind = diagind(A, 1)
+    sub_diag_ind = diagind(A, -1)
+
+    # Set the diagonals u_{ij} and coeffs for u_{j+1,i} and u_{j-1,i}, resp.
+    # NOTE: This is the same as sort of formulation as for laplace
+    # in the sense that -4uij + 1u{i+1, j} ...
+    A[diag_ind] .= D_xy
+    A[sup_diag_ind] .= D_y
+    A[sub_diag_ind] .= D_y
+
+    # Handle u_{i-1, j} and u_{i+1, j} coefficients
+
+    for j in 2:n_nodes-1 
+        throw("notimplemented")
+        for i in 1:n_nodes
+            A[i-1, j] = D_x
+            A[i+1, j] = D_x
+        end
+    end
+    
+    # Use the mesh to determine how the function `f` evaluates ...
+    A_row = 1 # flat index  
+    boundary_node_domain = (0, n_interior_points_in_one_direction+1)
+    for j in 1:n_interior_points_in_one_direction 
+        for i in 1:n_interior_points_in_one_direction
+ 
+            b[A_row] += f(i, j)
+
+            # TODO: Is this logic valid??? It's based on 11.3.1 essentially
+            # of Heath
+            # left boundary
+            if i-1 == 0
+                b[A_row] -= lbc/Δx^2
+            # right boundary
+            elseif i+1 == n_interior_points_in_one_direction+1
+                b[A_row] -= rbc/Δx^2
+            # bottom boundary
+            elseif j-1 == 0
+                b[A_row] -= bbc/Δy^2
+            # top boundary
+            else
+                b[A_row] -= tbc/Δy^2
+            end
+
+            A_row += 1  
+        end
+    end
+    return mesh, A, b 
+end 
