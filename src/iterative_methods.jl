28c8fe5ed3fe85eb8e75060b858e30c944d45819
Jared Frazier <cscidev001@gmail.com>
2024-02-02 13:36:29 +0100

    documented translation of CFD julia multigrid

--- src/iterative_methods.jl
+++ src/iterative_methods.jl
@@ -40,14 +40,14 @@ function jacobi_method(A::Matrix, b::Vector, x0::Vector, niters::Int)
         for i in 1:nrows
             prod_sum = 0
             for j in 1:ncols
-                i != j && (prod_sum += A[i, j]*xk[j])
+                i != j && (prod_sum += A[i, j] * xk[j])
             end
-            xkplus1[i] = (b[i] - prod_sum)/A[i, i]
+            xkplus1[i] = (b[i] - prod_sum) / A[i, i]
         end
         xk = xkplus1
     end
     return xkplus1
-end 
+end
 
 """
     jacobi_method_dolean(A::Matrix, b::Vector, x0::Vector, niters::Int)
@@ -74,9 +74,9 @@ function jacobi_method_dolean(A::Matrix, b::Vector, x0::Vector, niters::Int)
     D = diagm(D_vec)
     D_inv = inv(D)
     for k in 1:niters
-        rk = b - A*xk
-        xk = xk + D_inv*rk
-    end  
+        rk = b - A * xk
+        xk = xk + D_inv * rk
+    end
     return xk
 end
 
@@ -91,7 +91,7 @@ function jacobi_method_matrix_form_solver(
     U = copy(UpperTriangular(A)) # constructs a view, copy to avoid change A
     U[diagind(U)] .= 0
     for k in 1:niters
-        x = D_inv*(b - (L + U)*x) 
+        x = D_inv * (b - (L + U) * x)
     end
     return x
 end
@@ -103,9 +103,9 @@ function block_jacobi_method(
     m, n = size(A)
     @assert m >= p_procs
     remaining_rows = mod(m, p_procs)
-    
+
     restriction_matrices = nothing
-end 
+end
 
 """
     block_diagonal_matrix(A::Matrix, block_size::Int = 2)
@@ -133,19 +133,19 @@ julia> block_diagonal_matrix(A)
 # References
 [1] : https://phtournier.pages.math.cnrs.fr/5mm29/blockjacobi/
 """
-function block_diagonal_matrix(A::Matrix, block_size::Int = 2)
+function block_diagonal_matrix(A::Matrix, block_size::Int=2)
     m, n = size(A)
     @assert m == n "square matrix"
     M = zeros(m, n)
     diagonal_col_ix = 1
     for i in 1:m
-        diagonal_col_ix += (i != 1 && isodd(i))*block_size
+        diagonal_col_ix += (i != 1 && isodd(i)) * block_size
         for j in diagonal_col_ix:diagonal_col_ix+(block_size-1)
-           M[i, j] = A[i, j]
+            M[i, j] = A[i, j]
         end
     end
-    return M 
-end 
+    return M
+end
 
 """
     gauss_seidel_matheq(A::Matrix, b::Vector, x0::Vector, niters::Int; ω = 1.0)
@@ -157,7 +157,7 @@ for ω != 1 and ω ∈ (0, 2)) method.
 [1] : Ch. 11.5.3 Heath.
 """
 function gauss_seidel_matheq(
-    A::Matrix, b::Vector, x0::Vector, niters::Int; ω = 1.0)
+    A::Matrix, b::Vector, x0::Vector, niters::Int; ω=1.0)
     @assert 0 < ω < 2
     x = x0
     m, n = size(A)
@@ -165,19 +165,19 @@ function gauss_seidel_matheq(
         for i in 1:n
             xkplus1_sum = 0
             for j in 1:i-1
-                xkplus1_sum += A[i, j]*x[j]
+                xkplus1_sum += A[i, j] * x[j]
             end
-        
+
             xk_sum = 0
             for j in i+1:n
-                xk_sum += A[i, j]*x[j]
-            end 
+                xk_sum += A[i, j] * x[j]
+            end
 
-            x[i] = x[i] + ω*((b[i] - xkplus1_sum - xk_sum)/A[i,i] - x[i])
+            x[i] = x[i] + ω * ((b[i] - xkplus1_sum - xk_sum) / A[i, i] - x[i])
         end
     end
     return x
-end 
+end
 
 
 """
@@ -209,19 +209,104 @@ function gauss_seidel_matrix_form_solver(
 
     # gauss seidel iterations
     for k in 1:niters
-        x = D_plus_L_inv*(b - U*x)
-    end     
+        x = D_plus_L_inv * (b - U * x)
+    end
 
     return x
-end 
+end
 
-function ssor_preconditioner(A_::Matrix, ω::Float64 = 1.0)
+function ssor_preconditioner(A_::Matrix, ω::Float64=1.0)
     @assert issymmetric(A_)
     @assert 0 < ω < 2
     A = copy(A_) # expensive, whatevs
-    D = Diagonal(A) 
+    D = Diagonal(A)
     L = LowerTriangular(A)
     Lᵀ = transpose(L)
-    M = (1/(2 - ω))*((1/ω)*D + L)*inv(((1/ω)*D))*transpose((1/ω)*D + L)
+    M = (1 / (2 - ω)) * ((1 / ω) * D + L) * inv(((1 / ω) * D)) * transpose((1 / ω) * D + L)
     return M
+end
+
+"""
+    cg_poisson(nx, ny, un, r, max_iter, init_rms = nothing)
+
+Conjugate gradient method for 2D Poisson equation. 
+
+# Arguments
+- `nx, ny`: number of grid points in x and y directions
+- `nx, ny`: grid spacing in x and y directions
+- `un`: numerical solution matrix for the Poisson equation
+- `r`: residual matrix
+- `max_iter`: maximum number if iterations
+- `init_rms`: L-2 norm of the residual at the start of iterations
+
+# References
+[1] : Listing 22 in CFD Julia: CFD Julia: A Learning Module Structuring an 
+Introductory Course on Computational Fluid Dynamics
+"""
+function cg_poisson(nx, ny, un, r, max_iter, initial_rms)
+    p = r # assign initial residual to the conjugate vector
+    for k = 1:max_iter
+
+        # conjugate gradient algorithm
+        for j = 2:ny
+            for i = 2:nx
+                # centered difference formulas for second order deriv in x and y dir
+                q[i, j] = (p[i+1, j] - 2.0 * p[i, j] + p[i-1, j]) / (dx^2) +
+                        (p[i, j+1] - 2.0 * p[i, j] + p[i, j-1]) / (dy^2)
+            end
+        end
+
+        aa, bb = 0.0, 0.0
+        for j = 2:ny
+            for i = 2:nx
+                aa = aa + r[i, j] * r[i, j] # <r,r>
+                bb = bb + q[i, j] * p[i, j] # <q,p>
+            end
+        end
+
+        cc = aa / (bb + tiny) #alpha
+        for j = 2:ny
+            for i = 2:nx
+                un[i, j] = un[i, j] + cc * p[i, j] # update the numerical solution
+            end
+        end
+
+        bb, aa = aa, 0.0
+        for j = 2:ny
+            for i = 2:nx
+                r[i, j] = r[i, j] - cc * q[i, j] # update the residual
+                aa = aa + r[i, j] * r[i, j]
+            end
+        end
+
+        cc = aa / (bb + tiny) # beta
+        for j = 1:ny
+            for i = 1:nx
+                p[i, j] = r[i, j] + cc * p[i, j] # update the conjugate vector
+            end
+        end
+
+        # compute the residual at k-th step using the new solution
+        for j = 2:ny
+            for i = 2:nx
+                d2udx2 = (un[i+1, j] - 2 * un[i, j] + un[i-1, j]) / (dx^2)
+                d2udy2 = (un[i, j+1] - 2 * un[i, j] + un[i, j-1]) / (dy^2)
+                r[i, j] = f[i, j] - d2udx2 - d2udy2
+            end
+        end
+
+        # calculate the L-2 norm of the residual vector
+        rms = 0.0
+        for j = 2:ny
+            for i = 2:nx
+                rms = rms + r[i, j]^2
+            end
+        end
+
+        rms = sqrt(rms / ((nx - 1) * (ny - 1)))
+        # if the convergence critera (rms<tolerance) is satidfied, stop the iteration
+        if (rms / initial_rms) <= 1e-12
+            break
+        end
+    end
 end 
--- src/multigrid.jl
+++ src/multigrid.jl
@@ -1,8 +1,195 @@
-# Multigrid preconditioner for solution to equations arising
+# Multigrid preconditioner.solver for solution to equations arising
 # from finite difference discretization of Laplace (?) equation.
+# CFD Julia: CFD Julia: A Learning Module Structuring an
+# Introductory Course on Computational Fluid Dynamics
 
-function multigrid()
+"""
+    multigrid!(
+        dx, dy, nxf::Int, nyf::Int, nxc::Int, nyc::Int, v1::Int, v2::Int, v3::Int, 
+        unf::AbstractMatrix, unc::AbstractMatrix, f::AbstractMatrix,
+        ef::AbstractMatrix, ec::AbstractMatrix, r::AbstractMatrix,
+        tol = 1e-12)
 
-end 
+# Examples 
+```julia-repl
+julia> # TODO 
+```
 
+# Arguments 
+- `dx,dy`: grid spacing in x and y direction for fine grid.
+- `nxf, nyf`: number of grid points in x and y directions on fine grid.
+- `nxc, nyc`: number of grid points in x and y directions on coarse grid
+(nxc = nxf/2, nyc = nyf/2).
+- `v1, v2, v3`: number of iterations in relaxation step for different grid levels.
+- `unf`: numerical solution on fine grid (required solution).
+- `unc`: solution to the error correction on coarse grid.
+- `f`: source term. 
+- `ef`: error correction on fine grid.
+- `ec`: error correction on coarse grid. 
+- `r`: residual of fine grid. 
+- `tol`: Tolerance for convergence. 
+"""
+function multigrid!(
+    dx, dy, nxf::Int, nyf::Int, nxc::Int, nyc::Int, v1::Int, v2::Int, v3::Int, 
+    unf::AbstractMatrix, unc::AbstractMatrix, f::AbstractMatrix,
+    ef::AbstractMatrix, ec::AbstractMatrix, r::AbstractMatrix,
+    tol = 1e-12)
+    for k = 1:max_iter
 
+        # call relaxation on fine grid and compute the numerical solution
+        gauss_seidel_mg!(nxf, nyf, dx, dy, f, unf, v1)
+
+        # compute the residual and L2 norm
+        compute_residual!(nxf, nyf, dx, dy, f, unf, r)
+        rms = compute_l2norm(nxf, nyf, r)
+        if (rms / init_rms) <= tol 
+            break
+        end
+
+        # restrict the residual from fine level to coarse level
+        restriction!(nxc, nyc, r, ec)
+
+        # set solution zero on coarse grid
+        #unc[:, :] = zeros(nxc + 1, nyc + 1)
+        unc[:, :] .= 0
+
+        # solve on the coarsest level and relax V3 times
+        gauss_seidel_mg!(nxc, nyc, 2.0 * dx, 2.0 * dy, fc, unc, v3)
+
+        # prolongate solution from coarse level to fine level
+        prolongation!(nxc, nyc, unc, ef)
+
+        # correct the solution on fine level
+        for j = 2:nyf
+            for i = 2:nxf
+                unf[i, j] = unf[i, j] + ef[i, j]
+            end
+        end
+
+        # relax v2 times
+        gauss_seidel_mg!(nxf, nyf, dx, dy, f, unf, v2)
+    end
+    return nothing
+end
+
+"""
+    restriction(nxc, nyc, r, ec)
+
+Project the residuals on the fine grid onto the errors of the coarse grid. 
+
+# Arguments
+- `nxf, nyf`: UNUSED. number of grid points in x and y directions on fine grid. 
+- `nxc, nyc`: number of grid points in x and y directions on coarse grid
+    (nxc = nxf/2, nyc = nyf/2).
+- `r`: residual matrix for the Poisson equation on fine grid.
+- `ec`: error correction on coarse grid.
+"""
+function restriction!(nxc, nyc, r, ec)
+    for j = 2:nyc
+        for i = 2:nxc
+            # grid index for fine grid for the same coarse point
+            center = 4.0 * r[2*i-1, 2*j-1]
+            # E, W, N, S with respect to coarse grid point in fine grid
+            grid = 2.0 * (r[2*i-1, 2*j-1+1] + r[2*i-1, 2*j-1-1] +
+                          r[2*i-1+1, 2*j-1] + r[2*i-1-1, 2*j-1])
+            # NE, NW, SE, SW with respect to coarse grid point in fine grid
+            corner = 1.0 * (r[2*i-1+1, 2*j-1+1] + r[2*i-1+1, 2*j-1-1] +
+                            r[2*i-1-1, 2*j-1+1] + r[2*i-1-1, 2*j-1-1])
+            # restriction using trapezoidal rule
+            ec[i, j] = (center + grid + corner) / 16.0
+        end
+    end
+    return nothing
+end
+
+"""
+    prolongation(nxc, nyc, unc, ef)
+
+Project the errors on the the coarse grid onto the fine grid.
+
+# Arguments 
+- `nxf, nyf`: UNUSED. number of grid points in x and y directions on fine grid.
+- `nxc, nyc`: number of grid points in x and y directions on coarse grid
+(nxc = nxf/2, nyc = nyf/2).
+- `unc`: solution to the error correction on coarse grid.
+- `ef`: error correction on fine grid.
+"""
+function prolongation!(nxc, nyc, unc, ef)
+    for j = 1:nyc
+        for i = 1:nxc
+            # direct injection at center point
+            ef[2*i-1, 2*j-1] = unc[i, j]
+            # east neighnour on fine grid corresponding to coarse grid point
+            ef[2*i-1, 2*j-1+1] = 0.5 * (unc[i, j] + unc[i, j+1])
+            # north neighbout on fine grid corresponding to coarse grid point
+            ef[2*i-1+1, 2*j-1] = 0.5 * (unc[i, j] + unc[i+1, j])
+            # north-east neighbour on fine grid corresponding to coarse grid point
+            ef[2*i-1+1, 2*j-1+1] = 0.25 * (unc[i, j] + unc[i, j+1] +
+                                           unc[i+1, j] + unc[i+1, j+1])
+        end
+    end
+    return nothing
+end
+
+"""
+    gauss_seidel_mg!(nx, ny, dx, dy, f, un, v)
+
+Gauss-Seidel iterative solver (i.e., smoother, relaxation operator).
+
+# Arguments 
+- `nx, ny`: number of grid points in x and y directions.
+- `dx, dy`: grid spacing in x and y directions.
+- `un`: relaxation solution after fixed number of iterations.
+- `f`: source term.
+- `v`: number of iterations.
+"""
+function gauss_seidel_mg!(nx, ny, dx, dy, f, un, v)
+    rt = zeros(Float64, nx + 1, ny + 1) # temporary variable
+    den = -2.0 / dx^2 - 2.0 / dy^2
+    for iteration_count = 1:v
+        for j = 2:nx
+            for i = 2:ny
+                rt[i, j] = f[i, j] - (un[i+1, j] - 2.0 * un[i, j] + un[i-1, j]) / dx^2
+                -(un[i, j+1] - 2.0 * un[i, j] + un[i, j-1]) / dy^2
+                un[i, j] = un[i, j] + rt[i, j] / den
+            end
+        end
+    end
+    return nothing
+end
+
+"""
+    compute_residual!(
+        nx::Int, ny::Int, dx, dy,
+        f::AbstractMatrix, un::AbstractMatrix, r::AbstractMatrix)
+
+Compute the residual at k-th step using the new solution
+"""
+function compute_residual!(
+    nx::Int, ny::Int, dx, dy,
+    f::AbstractMatrix, un::AbstractMatrix, r::AbstractMatrix)
+    for j = 2:ny
+        for i = 2:nx
+            d2udx2 = (un[i+1, j] - 2 * un[i, j] + un[i-1, j]) / (dx^2)
+            d2udy2 = (un[i, j+1] - 2 * un[i, j] + un[i, j-1]) / (dy^2)
+            r[i, j] = f[i, j] - d2udx2 - d2udy2
+        end
+    end
+    return nothing
+end
+
+"""
+    compute_l2norm(mx, ny, r::AbstractMatrix)
+
+Calculate the L-2 norm of the residual vector
+"""
+function compute_l2norm(nx, ny, r::AbstractMatrix)
+    rms = 0.0
+    for j = 2:ny
+        for i = 2:nx
+            rms = rms + r[i, j]^2
+        end
+    end
+    rms = sqrt(rms / ((nx - 1) * (ny - 1)))
+    return rms 
+end
